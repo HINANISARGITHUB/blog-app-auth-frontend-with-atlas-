e97add72e42458be553acdaaa6e1d62170853f77
HINANISARGITHUB <hinanisar7860@gmail.com>
2026-05-06 08:13:16 +0500

    code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import "./App.css"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import PrivateRoute from './components/PrivateRoutes';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoutes";
 export default function App() {
- return (
- <BrowserRouter>
- <AuthProvider>
- <Routes>
- <Route path='/' element={<Login />} />
- <Route path='/login' element={<Login />} />
- <Route path='/register' element={<Register />} />
- <Route path='/dashboard' element={
- <PrivateRoute><Dashboard /></PrivateRoute>
- } />
- </Routes>
- </AuthProvider>
- </BrowserRouter>
- );
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
--- src/api/axios.js
+++ src/api/axios.js
@@ -3,3 +3,4 @@ export default axios.create({
  baseURL: import.meta.env.VITE_API_URL,
  withCredentials: true,
 });
+
